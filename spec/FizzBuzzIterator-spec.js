a8c4ba8e30ca844d9afbf78bbf6dac76e4e4b918
Will Grange <will.grange@gmail.com>
2014-08-22 21:39:49 +0100

    Nested decorators.

--- spec/FizzBuzzIterator-spec.js
+++ spec/FizzBuzzIterator-spec.js
@@ -1,11 +1,28 @@
-function NumberOutputStrategy()
+
+
+function FizzBuzzNumberOutputStrategyDecorator(numberStrategy)
 {
+	this.numberStrategy = numberStrategy;
+
 	this.output = function(number)
 	{
-		return (number % 3 == 0) ? "fizz" : number;
+		var output = this.numberStrategy.output(number);
+
+		return ((output % 5) == 0) && ((output % 3) == 0) ? "fizzbuzz" : output;
 	}
 }
 
+function FizzNumberOutputStrategyDecorator(numberStrategy)
+{
+	this.numberStrategy = numberStrategy;
+
+	this.output = function(number)
+	{
+		var output = this.numberStrategy.output(number);
+
+		return (number % 3) == 0 ? "fizz" : output;
+	}
+}
 
 function BuzzNumberOutputStrategyDecorator(numberStrategy)
 {
@@ -13,65 +30,93 @@ function BuzzNumberOutputStrategyDecorator(numberStrategy)
 
 	this.output = function(number)
 	{
-
 		var output = this.numberStrategy.output(number);
 
-		return (output % 5 == 0) ? "buzz" : output;;
+		return (output % 5) == 0 ? "buzz" : output;
 	}
 }
 
+function NumberOutputStrategy()
+{
+	this.output = function(number)
+	{
+		return number;
+	}
+}
 
-function FizzBuzzIterator(outputStrategy)
+function FizzBuzzGenerator(outputStrategy)
 {
 	this.outputStrategy = outputStrategy;
 
-	this.play = function()
+	this.generate = function()
 	{
 		var outputs = new Array();
-		for (var i = 1; i <= 100; i++){
+		for (var i = 1; i <= 100; i++)
+		{
 			outputs.push(this.outputStrategy.output(i));
-		};
+		}
 
 		return outputs;
 	}
 }
 
-describe("FizzBuzzIterator", function() {
+function NumberOutputStrategyBuilder()
+{
+	this.numberOutputStrategy = function() 
+	{
+		return new FizzNumberOutputStrategyDecorator
+		(
+			new BuzzNumberOutputStrategyDecorator
+			(
+				new FizzBuzzNumberOutputStrategyDecorator
+				(
+					new NumberOutputStrategy()
+				)
+			)
+		);
+	}
+}
+
+describe("FizzBuzzGenerator", function() {
 	
 	
 	var iterator = null;
 	beforeEach(function(){
 
-		var strategy = new BuzzNumberOutputStrategyDecorator(new NumberOutputStrategy());
-		iterator = new FizzBuzzIterator(strategy);
-
+		var strategy = new NumberOutputStrategyBuilder().numberOutputStrategy();
+		iterator = new FizzBuzzGenerator(strategy);
 	});
 
 	it("should create a new iterator", function() {
 		
 		expect(iterator).toNotBe(null);
-
 	});
 
 	it("should return an array of length 100", function() {
 
-		var expected = iterator.play();
+		var expected = iterator.generate();
 
 		expect(expected.length).toBe(100);
-	
 	});
 
 	it("should return fizz when we get the 3rd item", function() {
 		
-		var expected = iterator.play();
+		var expected = iterator.generate();
 
 		expect(expected[2]).toBe("fizz");
 	});
 
 	it("should return buzz when we get the 5th item", function() {
 
-		var expected = iterator.play();
+		var expected = iterator.generate();
 
 		expect(expected[4]).toBe("buzz");
+	});
+
+	it("should return fizzbuzz when we get the 15th item", function() {
+
+		var expected = iterator.generate();
+
+		expect(expected[14]).toBe("fizzbuzz");
 	});
 });
