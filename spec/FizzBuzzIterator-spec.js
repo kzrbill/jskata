7575ed19ef7a43a38b533872c0915d20de28822a
Will Grange <will.grange@gmail.com>
2014-10-22 20:30:12 +0100

    Extracted implementation out to node module.

--- spec/FizzBuzzIterator-spec.js
+++ spec/FizzBuzzIterator-spec.js
@@ -1,74 +1,8 @@
-function NumberOutputGenerator(outputStrategy)
-{
-	this.outputStrategy = outputStrategy;
+var module = require('../code/FizzBuzzIterator.js');
 
-	this.generate = function()
-	{
-		var outputs = new Array();
-		for (var i = 1; i <= 100; i++)
-		{
-			outputs.push(this.outputStrategy.output(i));
-		}
-
-		return outputs;
-	}
-}
-
-function NumberOutputStrategy()
-{
-	this.output = function(number)
-	{
-		return number;
-	}
-}
-
-function FizzOutputStrategyDecorator(outputStrategy)
-{
-	this.outputStrategy = outputStrategy;
-
-	this.output = function(number)
-	{
-		return number % 3 == 0 ? "fizz" : this.outputStrategy.output(number);
-	}
-}
-
-function BuzzOutputStrategyDecorator(outputStrategy)
-{
-	this.outputStrategy = outputStrategy;
-
-	this.output = function(number)
-	{
-		return number % 5 == 0 ? "buzz" : this.outputStrategy.output(number);
-	}
-}
-
-function FizzBuzzOutputStrategyDecorator(outputStrategy)
-{
-	this.outputStrategy = outputStrategy;
-
-	this.output = function(number)
-	{
-		return (number % 5 == 0) && (number % 3 == 0) ?
-			"fizzbuzz" : this.outputStrategy.output(number);
-	}
-}
-
-function FizzBuzzOutputStrategyBuilder()
-{
-	this.outputStrategy = function() 
-	{
-		return new FizzBuzzOutputStrategyDecorator
-		(
-			new BuzzOutputStrategyDecorator
-			(
-				new FizzOutputStrategyDecorator
-				(
-					new NumberOutputStrategy()
-				)
-			)
-		);
-	}
-}
+var FizzBuzzOutputStrategyBuilder = module.FizzBuzzOutputStrategyBuilder;
+var NumberOutputGenerator 				= module.NumberOutputGenerator;
+var NumberOutputStrategy          = module.NumberOutputStrategy;
 
 describe("NumberOutputGenerator", function() {
 	
